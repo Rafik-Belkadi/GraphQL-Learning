46a96adbb0cabbb17493cf1fd6a7c044be04cdc3
Mohamed Belkadi <mohamed.belkadi@MacBook-Pro-de-Mohamed.local>
2019-04-21 16:14:26 +0100

    Added Schema file, mutations and queries

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,41 +1,46 @@
 const { GraphQLServer } = require('graphql-yoga')
 
 // 1
-const typeDefs = `
-    type Query {
-        info: String!
-        feed: [Link!]!
-    }
 
-    type Link {
-        id: ID!
-        description: String!
-        url: String!
-    }
-`
 let links = [{
     id: 'link-0',
     url: 'www.howtographql.com',
     description: 'Fullstack tutorial for GraphQL'
 }]
 
+let idCount = links.length
 // 2
 const resolvers = {
    Query: {
        info: () => 'This is the API of a Hackernews Clone',
-       //2
-       feed: () => links
+       feed: () => links,
+       byid: (_,{id}) => links[id]
    },
-   // 3
-   Link: {
-       id: (parent) => parent.id,
-       description: (parent) => parent.description,
-       url: (parent) => parent.url
+   Mutation: {
+       post: (parent,args) => {
+           const link = {
+               id: `link-${idCount++}`,
+               description: args.description,
+               url: args.url
+           }
+           links.push(link)
+           return link
+       },
+       updateLink: (parent,args) => {
+            const selected = links[args.id]
+            selected.url = args.url
+            selected.description = args.description
+            return selected
+       },
+       deleteLink: (parent,args) => {
+            const selected = links.pop(args.id)
+            return selected
+       }
    }
 }
 
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers
 })
 server.start( () => console.log(`Server is running on http://localhost:4000`))
